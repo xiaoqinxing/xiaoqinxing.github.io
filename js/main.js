23f8265dd638ff0b71e02cf9ad3d3cdb5fa4a93e
xiaoqinxing <1415289814@qq.com>
2020-03-28 20:09:28 +0800

    Site updated: 2020-03-28 20:09:09

--- js/main.js
+++ js/main.js
@@ -271,16 +271,16 @@ $(function () {
    * 代碼收縮
    */
   const $highlightTools = $('.highlight-tools')
-  if (isHighlightShrink === 'true') {
+  if (isHighlightShrink === true) {
     $highlightTools.append('<i class="fa fa-angle-down code-expand code-closed" aria-hidden="true"></i>')
-  } else if (isHighlightShrink === 'false') {
+  } else if (isHighlightShrink === false) {
     $highlightTools.append('<i class="fa fa-angle-down code-expand" aria-hidden="true"></i>')
   }
 
   $(document).on('click', '.highlight-tools >.code-expand', function () {
     var $table = $(this).parent().next()
     if ($(this).hasClass('code-closed')) {
-      $table.css('display', 'block')
+      $table.css('display', '')
       $(this).removeClass('code-closed')
     } else {
       $table.css('display', 'none')
@@ -668,7 +668,8 @@ $(function () {
   var isFontAwesomeV5 = GLOBAL_CONFIG.isFontAwesomeV5
   var $darkModeButtom = $('#darkmode')
   if (typeof autoChangeMode !== 'undefined') {
-    document.documentElement.getAttribute('data-theme') === 'dark' ? changeLightIcon() : changeDarkIcon()
+    if (Cookies.get('theme') === 'dark') changeLightIcon()
+    else changeDarkIcon()
   }
 
   function changeLightIcon () {
